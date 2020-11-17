1d36a22851612683d9df11ded62a21266613e710
Miquel Bellet <miquel.bellet@gmail.com>
2020-11-17 23:45:02 +0100

    level editor

--- Assets/Scripts/EditLevelController.cs
+++ Assets/Scripts/EditLevelController.cs
@@ -4,17 +4,102 @@ using UnityEngine;
 
 public class EditLevelController : MonoBehaviour
 {
-    private LevelScript levelScript;
+    public GameObject selectedItemPrefab;
+    public Sprite wallSprite;
+    public Sprite boxSprite;
+    public Sprite targetSprite;
 
+
+    private LevelScript levelScript;
+    private int Cols = 8;
+    private int Rows = 12;
+    int[,] cells;
+
+    private GameObject selectedItemObject;
+    private bool settingWall;
+    private bool settingBox;
+    private bool settingTarget;
     void Start()
     {
         levelScript = GameObject.FindGameObjectWithTag("Level").GetComponent<LevelScript>();
-        var numRows = levelScript.numRows;
-        var numCols = levelScript.numCols;
-        int[,] cells = new int[numRows, numCols];
-        for (int i = 0; i < numRows; i++)
+        //if (PlayerPrefs.GetInt("EditLevel", 0) > 0) DrawSelectedLevel();
+        //else DrawEmptyLevel();
+        DrawSelectedLevel();
+    }
+
+    void Update()
+    {
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var xPos = Mathf.Round(mousePos.x + 4);
+        var yPos = Mathf.Round(mousePos.y + 4);
+        if (Input.GetMouseButtonDown(1))
         {
-            for (int n = 0; n < numCols; n++)
+            try
+            {
+                cells[(int)xPos, (int)yPos] = 3;
+                levelScript.DrawLevel(cells);
+            }
+            catch { }
+        }
+        if (settingWall)
+        {
+            var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            selectedItemObject.transform.position = new Vector3(mouseWorldPos.x, mouseWorldPos.y, 0);
+            if (Input.GetMouseButtonDown(0))
+            {
+                settingWall = false;
+                selectedItemObject.SetActive(false);
+                Destroy(selectedItemObject);
+                try
+                {
+                    cells[(int)xPos, (int)yPos] = 0;
+                    levelScript.DrawLevel(cells);
+                }
+                catch { }
+            }
+        }
+        else if (settingBox)
+        {
+            var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            selectedItemObject.transform.position = new Vector3(mouseWorldPos.x, mouseWorldPos.y, 0);
+            if (Input.GetMouseButtonDown(0))
+            {
+                settingBox = false;
+                selectedItemObject.SetActive(false);
+                Destroy(selectedItemObject);
+                try
+                {
+                    cells[(int)xPos, (int)yPos] = 1;
+                    levelScript.DrawLevel(cells);
+                }
+                catch { }
+            }
+        }
+        else if (settingTarget)
+        {
+            var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            selectedItemObject.transform.position = new Vector3(mouseWorldPos.x, mouseWorldPos.y, 0);
+            if (Input.GetMouseButtonDown(0))
+            {
+                settingTarget = false;
+                selectedItemObject.SetActive(false);
+                Destroy(selectedItemObject);
+                try
+                {
+                    cells[(int)xPos, (int)yPos] = 2;
+                    levelScript.DrawLevel(cells);
+                }
+                catch { }
+            }
+        }
+    }
+
+    private void DrawSelectedLevel()
+    {
+        cells = new int[Rows, Cols];
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int n = Cols - 1; n >= 0; n--)
             {
                 cells[i, n] = Random.Range(0, 4);
             }
@@ -22,8 +107,40 @@ public class EditLevelController : MonoBehaviour
         levelScript.DrawLevel(cells);
     }
 
-    void Update()
+    private void DrawEmptyLevel()
     {
-        
+        cells = new int[Rows, Cols];
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int n = Cols - 1; n >= 0; n--)
+            {
+                cells[i, n] = 3;
+            }
+        }
+        levelScript.DrawLevel(cells);
+    }
+
+    public void SetWall()
+    {
+        settingWall = true;
+        selectedItemObject = Instantiate(selectedItemPrefab);
+        selectedItemObject.SetActive(true);
+        selectedItemObject.GetComponent<SpriteRenderer>().sprite = wallSprite;
+    }
+
+    public void SetBox()
+    {
+        settingBox = true;
+        selectedItemObject = Instantiate(selectedItemPrefab);
+        selectedItemObject.SetActive(true);
+        selectedItemObject.GetComponent<SpriteRenderer>().sprite = boxSprite;
+    }
+
+    public void SetTarget()
+    {
+        settingTarget = true;
+        selectedItemObject = Instantiate(selectedItemPrefab);
+        selectedItemObject.SetActive(true);
+        selectedItemObject.GetComponent<SpriteRenderer>().sprite = targetSprite;
     }
 }
--- Assets/Scripts/LevelScript.cs
+++ Assets/Scripts/LevelScript.cs
@@ -4,29 +4,37 @@ using UnityEngine;
 
 public class LevelScript : MonoBehaviour
 {
-    [HideInInspector] public int[,] cells = new int[Rows, Cols]; // 0 = empty; 1 = wall; 2 = box; 3 = target
-    [HideInInspector] public static int Rows = 8;
-    [HideInInspector] public static int Cols = 10;
-    [HideInInspector] public int numRows = 8;
-    [HideInInspector] public int numCols = 10;
+    private int[,] levelCells; // 0 = empty; 1 = wall; 2 = box; 3 = target
 
     public GameObject emptyPrefab;
     public GameObject wallPrefab;
     public GameObject boxPrefab;
     public GameObject boxPlacedPrefab;
     public GameObject targetPrefab;
+    public GameObject editablePrefab;
 
     public void DrawLevel(int[,] cells)
     {
-        for (int i = 0; i < Rows; i++)
+        if(transform.childCount > 0)
         {
-            for (int n = 0; n < Cols; n++)
+            for (int i = 0; i < transform.childCount; i++)
             {
-                if(cells[i, n] == 0) { Instantiate(emptyPrefab, new Vector3(i, n, 0), Quaternion.Euler(0, 0, 0), transform); }
-                else if(cells[i, n] == 1) { Instantiate(wallPrefab, new Vector3(i, n, 0), Quaternion.Euler(0, 0, 0), transform); }
-                else if(cells[i, n] == 2) { Instantiate(boxPrefab, new Vector3(i, n, 0), Quaternion.Euler(0, 0, 0), transform); }
-                else if(cells[i, n] == 3) { Instantiate(targetPrefab, new Vector3(i, n, 0), Quaternion.Euler(0, 0, 0), transform); }
+                Destroy(transform.GetChild(i).gameObject);
             }
         }
+
+        levelCells = cells;
+        transform.position = new Vector2(0, 0);
+        for (int i = 0; i < cells.GetLength(0); i++)
+        {
+            for (int n = cells.GetLength(1) - 1; n >= 0; n--)
+            {
+                if(cells[i, n] == 0) { Instantiate(wallPrefab, new Vector3(i, n, 0), Quaternion.Euler(0, 0, 0), transform); }
+                else if(cells[i, n] == 1) { Instantiate(boxPrefab, new Vector3(i, n, 0), Quaternion.Euler(0, 0, 0), transform); }
+                else if(cells[i, n] == 2) { Instantiate(targetPrefab, new Vector3(i, n, 0), Quaternion.Euler(0, 0, 0), transform); }
+                else if(cells[i, n] == 3) { Instantiate(editablePrefab, new Vector3(i, n, 0), Quaternion.Euler(0, 0, 0), transform); }
+            }
+        }
+        transform.position = new Vector2(-4, -4);
     }
 }
